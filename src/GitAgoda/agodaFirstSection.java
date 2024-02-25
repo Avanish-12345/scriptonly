60d4757594e828dce558b7fb12a8d37a90bc1db1
Avanish-12345 <147083718+Avanish-12345@users.noreply.github.com>
2024-02-25 09:30:40 +0530

    select bangalore

--- src/GitAgoda/agodaFirstSection.java
+++ src/GitAgoda/agodaFirstSection.java
@@ -7,17 +7,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class agodaFirstSection 
 {
-public static void main(String[] args)
+public static void main(String[] args) throws InterruptedException
 {
 	WebDriver driver=new ChromeDriver();
 	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
 	driver.get("https://www.agoda.com/");
+	WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(40));
+	wait.until(ExpectedConditions.titleIs("Agoda Official Site | Free Cancellation & Booking Deals | Over 2 Million Hotels"));
+	Actions action=new Actions(driver);
 	WebElement poplinks=driver.findElement(By.xpath("//button[text()='No thanks']"));
 	poplinks.click();
+	WebElement selecttextfield=driver.findElement(By.xpath("//input[@aria-label='Enter a destination or property']"));
+	selecttextfield.click();
+	Thread.sleep(4000);
+	WebElement selectbanglore=driver.findElement(By.xpath("//div[contains(@class,'a7c88-box a7c88-text-inherit a7c88-fill-inherit')]//p[text()='Bangalore']"));
+	//action.moveToElement(selectbanglore).pause(4000).click().build().perform();
+	action.click(selectbanglore).pause(4000).build().perform();
 	
 }
 }
