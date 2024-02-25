88380d2630b7dcfbfe971a51a1597e42fdd8942e
Avanish-12345 <147083718+Avanish-12345@users.noreply.github.com>
2024-02-25 12:49:44 +0530

    selectdate

--- src/GitAgoda/agodaFirstSection.java
+++ src/GitAgoda/agodaFirstSection.java
@@ -2,6 +2,7 @@ package GitAgoda;
 
 import java.sql.Driver;
 import java.time.Duration;
+import java.time.LocalDateTime;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -19,17 +20,33 @@ public static void main(String[] args) throws InterruptedException
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
 	driver.get("https://www.agoda.com/");
-	WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(40));
-	wait.until(ExpectedConditions.titleIs("Agoda Official Site | Free Cancellation & Booking Deals | Over 2 Million Hotels"));
 	Actions action=new Actions(driver);
 	WebElement poplinks=driver.findElement(By.xpath("//button[text()='No thanks']"));
+	WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(40));
+	wait.until(ExpectedConditions.visibilityOf(poplinks));
 	poplinks.click();
 	WebElement selecttextfield=driver.findElement(By.xpath("//input[@aria-label='Enter a destination or property']"));
 	selecttextfield.click();
 	Thread.sleep(4000);
-	WebElement selectbanglore=driver.findElement(By.xpath("//div[contains(@class,'a7c88-box a7c88-text-inherit a7c88-fill-inherit')]//p[text()='Bangalore']"));
-	//action.moveToElement(selectbanglore).pause(4000).click().build().perform();
-	action.click(selectbanglore).pause(4000).build().perform();
+	WebElement selectbanglore=driver.findElement(By.xpath("//p[text()='Bangalore']"));
+	action.moveToElement(selectbanglore).pause(4000).click(selectbanglore).build().perform();
+	WebElement previousmonthclick=driver.findElement(By.xpath("//button[@aria-label='Previous Month']"));
+	action.click(previousmonthclick).pause(2000).build().perform();
+	LocalDateTime time=LocalDateTime.now();
+	int day=time.getDayOfMonth();
+	int year=time.getYear();
+	String frommonth=time.getMonth().toString().toLowerCase();
+	frommonth=(frommonth.substring(0,1)).toUpperCase()+frommonth.substring(1,frommonth.length());
+	System.out.println(day+" "+frommonth+" "+year);
+    int day2=time.plusDays(1).getDayOfMonth();
+    System.out.println(day2+" "+frommonth+" "+year);
+	WebElement fromday=driver.findElement(By.xpath("//div[text()='"+frommonth+" "+year+"']/..//span[text()="+day+"]"));
+	action.click(fromday).pause(2000).build().perform();
+	WebElement toDay=driver.findElement(By.xpath("//div[text()='"+frommonth+" "+year+"']/..//span[text()="+day2+"]"));
+	action.click(toDay).pause(2000).build().perform();
+	
+	
+	
 	
 }
 }
